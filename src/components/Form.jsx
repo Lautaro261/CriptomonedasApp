156f01007abab83d83fb190595dfaab69dfca771
Nahuel Lautaro Torres Loretto <112157731+Lautaro261@users.noreply.github.com>
2023-09-02 21:42:04 -0300

    hay un problema con el map

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View, StyleSheet, Pressable } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import axios from "axios";
 
 
 const Form =()=>{
+    const [moneda, setMoneda] = useState('')
+    const [cripto, setCripto]= useState('')
+    const [criptos, setCriptos] = useState('')
+
+    const obtenerMoneda = (moneda) =>{
+        setMoneda(moneda)
+    }
+
+    useEffect(()=>{
+        const consultaAPI =async()=>{
+            const url='https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+            const resultado = await axios.get(url)
+            console.log(resultado.data.Data)
+            setCriptos(resultado.data.Data)
+        }
+
+        consultaAPI()
+    },[])
 
     return(
         <View>
             <Text style={styles.label}>Moneda</Text>
+            <Picker 
+            selectedValue={moneda}
+            onValueChange={(moneda)=> obtenerMoneda(moneda)}
+            itemStyle
+            >
+                <Picker.Item label="Seleccione" value=''/>
+                <Picker.Item label="Dolar US" value='USD'/>
+                <Picker.Item label="Peso MEX" value='MXN'/>
+                <Picker.Item label="Euro" value='EU'/>
+                <Picker.Item label="Libra Esterlina" value='GBP'/>
+            </Picker>
             
             <Text style={styles.label}>Criptomoneda</Text>
+
+            <Picker 
+            selectedValue={cripto}
+            onValueChange={(cripto)=> obtenerMoneda(cripto)}
+            >
+                <Picker.Item label="Seleccione" value=''/>
+                { criptos.map(cripto =>(
+                    <Picker.Item key={cripto.CoinInfo.id} label={cripto.CoinInfo.FullName} value={cripto.CoinInfo.Name}/>
+                )) }
+            </Picker>
+            
         </View>
     )
 }
@@ -21,7 +63,8 @@ const styles = StyleSheet.create({
         textTransform:'uppercase',
         fontSize:18,
         marginVertical:15,
-        color:'#fff'
+        color:'#fff',
+        fontFamily:'Lato-Black'
     }
 
 })
